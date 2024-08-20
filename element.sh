136e367faae533d7eb9057733d6de165bcc737a1
Jacob Hexamer <jacobhexamer@gmail.com>
2024-08-20 22:15:54 +0000

    feat: added if statement to check for argument

--- element.sh
+++ element.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
+if [[ -n $1 ]]
+then
+  echo "Argument received."
+else
+  echo "Please provide an element as an argument."
+fi
