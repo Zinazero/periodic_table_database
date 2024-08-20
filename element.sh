b2f6ad2be617edb402e81e752807367fd10467ac
Jacob Hexamer <jacobhexamer@gmail.com>
2024-08-20 22:21:05 +0000

    feat: added logic for receving symbol and finding corresponding element properties

--- element.sh
+++ element.sh
@@ -25,7 +25,21 @@ INNER JOIN properties USING (atomic_number) INNER JOIN types USING (type_id) WHE
   #else if symbol
   elif [[ $1 =~ ^[a-zA-Z]$ ]]
   then
-    echo "This is a symbol."
+    #check if number matches symbol in database
+    SYMBOL_RESULT=$($PSQL "SELECT atomic_number, name, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements
+INNER JOIN properties USING (atomic_number) INNER JOIN types USING (type_id) WHERE symbol = '$1';")
+
+    if [[ -z $SYMBOL_RESULT ]]
+    then
+      #if not found
+      echo "I could not find that element in the database."
+    else
+      #if found
+      echo $"$SYMBOL_RESULT" | while IFS='|' read ATOMIC_NUMBER NAME TYPE MASS MELT BOIL
+      do
+        echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($1). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELT celsius and a boiling point of $BOIL celsius."
+      done
+    fi
   else
     echo "This is a string."
   fi
